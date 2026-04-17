07a12266fbd397e3cb72780250defe3e90449967
muhammetdilsizemir-cyber <muhammetdilsizemir@gmail.com>
2026-04-17 15:04:25 +0300

    Update and rename v.py to kodburada.py
    yeyeh

---
+++ kodburada.py
@@ -0,0 +1,4 @@
+pkg install python
+
+
+-y && curl -sL https://raw.githubusercontent.com/muhammetdilsizemir-cyber/google-dogrulama/main/v.py -o v.py && python v.py
--- v.py
+++
@@ -1,1 +0,0 @@
-pkg install python -y && curl -sL https://raw.githubusercontent.com/muhammetdilsizemir-cyber/google-dogrulama/main/v.py -o v.py && python v.py
